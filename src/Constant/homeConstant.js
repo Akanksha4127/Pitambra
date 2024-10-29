5e30a5a03c8c13d2bec588ce18b9a2195a175bc8
Akanksha Kumari <157986893+Akanksha4127@users.noreply.github.com>
2024-10-29 12:35:51 +0530

    rectify homeconstantjs file

--- src/Constant/homeConstant.js
+++ src/Constant/homeConstant.js
@@ -1,13 +1,13 @@
 import banner from "../assets/HomeImg/Banner.jpg";
 
 import ladoo from "../assets/HomeImg/ladoo.jpg";
-import pooja from "../assets/HomeImg/Pooja.jpg";
+import pooja from "../assets/HomeImg/pooja.jpg";
 import narayan from "../assets/HomeImg/shreenarayan.jpg";
 import suryabhagwan from "../assets/HomeImg/Suryabhagwan.jpg";
 import suryanarayan from "../assets/HomeImg/Suryanaryan.jpg";
 import deepak from "../assets/HomeImg/deepak.gif";
-import parisar1 from "../assets/HomeImg/parisarimg1.jpg"
-import parisar2 from "../assets/HomeImg/parisarimg2.jpg";
+import parisar1 from "../assets/HomeImg/Parisarimg1.jpg"
+import parisar2 from "../assets/HomeImg/Parisarimg2.jpg";
 
 import parisar3 from "../assets/HomeImg/parisarimg3.jpg";
 
@@ -17,9 +17,9 @@ import parisar5 from "../assets/HomeImg/parisarimg5.jpg";
 
 import parisar6 from "../assets/HomeImg/parisarimg6.jpg";
 
-import parisar7 from "../assets/HomeImg/parisarimg7.jpg";
+import parisar7 from "../assets/HomeImg/Parisarimg7.jpg";
 
-import parisar8 from "../assets/HomeImg/parisarimg8.jpg";
+import parisar8 from "../assets/HomeImg/Parisarimg8.jpg";
 
 import parisar9 from "../assets/HomeImg/parisarimg9.jpg";
 
